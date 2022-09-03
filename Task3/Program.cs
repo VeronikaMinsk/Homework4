4cd4c0aeca38dd6647aed41d457526d668e6bc66
Veronika <griva.verona@gmail.com>
2022-09-03 20:02:24 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+Array2(array);
+void Array2(int[] array)
+{
+    int size = array.Length;
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1,10);
+        if(i == 0)
+            Console.Write("[" + array[i] + ", ");
+        else if (i >= 1 && i <= size-2)
+            Console.Write(array[i] + ", ");
+        else if (i == size-1)
+            Console.WriteLine(array[i] + "]");
+    }
+}
